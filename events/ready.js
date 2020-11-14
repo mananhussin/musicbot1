0417cfee5a35c9eeaf65bccaa2eaf281673998e6
Khan manan <68549079+mananhussin@users.noreply.github.com>
2020-11-15 01:39:46 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const config = require(`../config/bot.json`)
 module.exports = async (client) => {
 
     //If the bot is ready it sends a message in the console
-    console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
+    console.log(`x!help| Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 
     //Game
     client.user.setActivity(config.game)
